1d74fb5940f648294ef9ce2b9fd61a45b77cb73f
JotaRdgz06 <julianrodriguezchaves@gmail.com>
2026-06-08 14:58:33 -0600

    Implementación Tipo

--- src/Logica/Tipo.java
+++ src/Logica/Tipo.java
@@ -1,5 +1,39 @@
 package Logica;
 
-public class Tipo {
+import java.io.Serializable;
 
+public class Tipo implements Serializable{
+	private int codigo;
+	private String nombre;
+	private Item item;
+	
+	public Tipo(int codigo, String nombre, Item item) {
+		this.codigo = codigo;
+		this.nombre = nombre;
+		this.item = item;
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Item getItem() {
+		return item;
+	}
+
+	public void setItem(Item item) {
+		this.item = item;
+	}
 }
