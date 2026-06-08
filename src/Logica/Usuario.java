747f19a4f385f3abdfd0fe5780f32a3db47625fd
JotaRdgz06 <julianrodriguezchaves@gmail.com>
2026-06-08 14:44:19 -0600

    Implementación Usuario

--- src/Logica/Usuario.java
+++ src/Logica/Usuario.java
@@ -1,5 +1,39 @@
 package Logica;
 
-public class Usuario {
+import java.io.Serializable;
 
+public class Usuario implements Serializable {
+	private String nombre;
+	private String telefono;
+	private String correo;
+	
+	public Usuario(String nombre, String telefono, String correo) {
+		this.nombre = nombre;
+		this.telefono = telefono;
+		this.correo = correo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getCorreo() {
+		return correo;
+	}
+
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
 }
