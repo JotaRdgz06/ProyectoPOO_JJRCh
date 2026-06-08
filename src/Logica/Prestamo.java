6905b2b9a8adff7d4393a2d2e378e784b0fd7f4a
JotaRdgz06 <julianrodriguezchaves@gmail.com>
2026-06-08 17:47:14 -0600

    Implementación Prestamo

--- src/Logica/Prestamo.java
+++ src/Logica/Prestamo.java
@@ -1,5 +1,50 @@
 package Logica;
 
-public class Prestamo {
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
+public class Prestamo implements Serializable {
+	private int codigo;
+    private Usuario usuario;
+    private List<Item> items;
+    private Alerta alerta;
+    
+	public Prestamo(int codigo, Usuario usuario) {
+		super();
+		this.codigo = codigo;
+		this.usuario = usuario;
+		this.items = new ArrayList<>();
+		this.alerta = null;
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	public Alerta getAlerta() {
+		return alerta;
+	}
+
+	public void setAlerta(Alerta alerta) {
+		this.alerta = alerta;
+	}
+
+	public List<Item> getItems() {
+		return items;
+	}
+    
+    
 }
