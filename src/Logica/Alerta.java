988ffcafbf974287adc5c087a175b3b1e83eb3e5
JotaRdgz06 <julianrodriguezchaves@gmail.com>
2026-06-08 22:08:56 -0600

    Implementación Alerta

--- src/Logica/Alerta.java
+++ src/Logica/Alerta.java
@@ -1,5 +1,31 @@
 package Logica;
 
-public class Alerta {
+import java.io.Serializable;
+import java.time.LocalDateTime;
 
+public class Alerta implements Serializable {
+	
+	private TipoAlerta tipo;
+    private LocalDateTime fechaActivacion;
+
+    public enum TipoAlerta {
+		UNA_VEZ, RECURRENTE
+	}
+    
+    public Alerta(TipoAlerta tipo, LocalDateTime fechaActivacion) {
+        this.tipo = tipo;
+        this.fechaActivacion = fechaActivacion;
+    }
+	
+    public TipoAlerta getTipo() {
+    	return tipo;
+    }
+    
+    public void setTipo(TipoAlerta tipo) {
+    	this.tipo = tipo;
+    }
+    
+    public LocalDateTime getFechaActivacion() {
+    	return fechaActivacion;
+    }
 }
