339247b50be0d700ecac3eb7cea58639cdb75fe8
JotaRdgz06 <julianrodriguezchaves@gmail.com>
2026-06-08 20:15:18 -0600

    Implementación Categoria

--- src/Logica/Categoria.java
+++ src/Logica/Categoria.java
@@ -1,5 +1,30 @@
 package Logica;
 
-public class Categoria {
+import java.io.Serializable;
 
+public class Categoria implements Serializable{
+	
+	private int codigo;
+	private String nombre;
+	
+	public Categoria(int codigo, String nombre) {
+		this.codigo = codigo;
+		this.nombre = nombre;
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
 }
