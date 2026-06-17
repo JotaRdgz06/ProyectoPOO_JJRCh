bc19dc9d713899b8a5cb83a28c4eced53e572599
JotaRdgz06 <julianrodriguezchaves@gmail.com>
2026-06-16 19:00:58 -0600

    Tipo avance 3

--- src/Logica/Tipo.java
+++ src/Logica/Tipo.java
@@ -1,22 +1,28 @@
 package Logica;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Tipo implements Serializable{
 	private int codigo;
 	private String nombre;
 	private boolean esGenerico;
+	private List<Item> items;
+
 	
 	public Tipo(int codigo, String nombre) {
 		this.codigo = codigo;
 		this.nombre = nombre;
 		this.esGenerico = false;
+		this.items = new ArrayList<>();
 	}
 
 	public Tipo(int codigo, String nombre, boolean esGenerico) {
 		this.codigo = codigo;
         this.nombre = nombre;
         this.esGenerico = esGenerico;
+        this.items = new ArrayList<>();
 	}
 	
 	public int getCodigo() {
@@ -41,5 +47,19 @@ public class Tipo implements Serializable{
 	
 	public boolean puedeEliminarse() {
         return !esGenerico;
+    }
+	
+	public List<Item> getItems() {
+        return items;
+    }
+ 
+    public void agregarItem(Item item) {
+        if (!items.contains(item)) {
+            items.add(item);
+        }
+    }
+ 
+    public void eliminarItem(Item item) {
+        items.remove(item);
     }
 }
