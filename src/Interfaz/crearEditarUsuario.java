2fd56893f6524d9132b83093cad48b9bb7fcacf1
JotaRdgz06 <julianrodriguezchaves@gmail.com>
2026-06-18 18:53:34 -0600

    crearEditarUsuario avance 1

--- src/Interfaz/crearEditarUsuario.java
+++ src/Interfaz/crearEditarUsuario.java
@@ -7,11 +7,16 @@ import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class crearEditarUsuario extends JDialog {
 
 	private static final long serialVersionUID = 1L;
 	private final JPanel contentPanel = new JPanel();
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
 
 	/**
 	 * Launch the application.
@@ -30,11 +35,42 @@ public class crearEditarUsuario extends JDialog {
 	 * Create the dialog.
 	 */
 	public crearEditarUsuario() {
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 305, 224);
 		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setLayout(new FlowLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Nombre:");
+		lblNewLabel.setBounds(10, 10, 50, 24);
+		contentPanel.add(lblNewLabel);
+		
+		textField = new JTextField();
+		textField.setBounds(70, 13, 156, 18);
+		contentPanel.add(textField);
+		textField.setColumns(10);
+		{
+			JLabel lblCorreo = new JLabel("Correo:");
+			lblCorreo.setBounds(10, 67, 50, 24);
+			contentPanel.add(lblCorreo);
+		}
+		{
+			textField_1 = new JTextField();
+			textField_1.setColumns(10);
+			textField_1.setBounds(70, 70, 156, 18);
+			contentPanel.add(textField_1);
+		}
+		{
+			JLabel lblTelfono = new JLabel("Teléfono:");
+			lblTelfono.setBounds(10, 122, 66, 24);
+			contentPanel.add(lblTelfono);
+		}
+		{
+			textField_2 = new JTextField();
+			textField_2.setColumns(10);
+			textField_2.setBounds(70, 125, 156, 18);
+			contentPanel.add(textField_2);
+		}
 		{
 			JPanel buttonPane = new JPanel();
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
@@ -52,5 +88,4 @@ public class crearEditarUsuario extends JDialog {
 			}
 		}
 	}
-
 }
