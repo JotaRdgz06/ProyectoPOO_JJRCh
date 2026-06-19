90c5d53225c5658f0225f90596fa224978b1b2f7
JotaRdgz06 <julianrodriguezchaves@gmail.com>
2026-06-18 18:20:30 -0600

    pantallaTipos avance 1

--- src/Interfaz/pantallaTipos.java
+++ src/Interfaz/pantallaTipos.java
@@ -7,11 +7,15 @@ import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JTable;
+import javax.swing.JScrollPane;
+import javax.swing.table.DefaultTableModel;
 
 public class pantallaTipos extends JDialog {
 
 	private static final long serialVersionUID = 1L;
 	private final JPanel contentPanel = new JPanel();
+	private JTable table;
 
 	/**
 	 * Launch the application.
@@ -32,9 +36,55 @@ public class pantallaTipos extends JDialog {
 	public pantallaTipos() {
 		setBounds(100, 100, 450, 300);
 		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setLayout(new FlowLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		{
+			JScrollPane scrollPane = new JScrollPane();
+			scrollPane.setBounds(10, 10, 416, 166);
+			contentPanel.add(scrollPane);
+			{
+				table = new JTable();
+				table.setModel(new DefaultTableModel(
+					new Object[][] {
+					},
+					new String[] {
+						"C\u00F3digo", "Nombre"
+					}
+				) {
+					Class[] columnTypes = new Class[] {
+						String.class, String.class
+					};
+					public Class getColumnClass(int columnIndex) {
+						return columnTypes[columnIndex];
+					}
+					boolean[] columnEditables = new boolean[] {
+						false, false
+					};
+					public boolean isCellEditable(int row, int column) {
+						return columnEditables[column];
+					}
+				});
+				table.getColumnModel().getColumn(0).setResizable(false);
+				table.getColumnModel().getColumn(1).setResizable(false);
+				scrollPane.setViewportView(table);
+			}
+		}
+		{
+			JButton btnNewButton = new JButton("Crear");
+			btnNewButton.setBounds(10, 202, 84, 20);
+			contentPanel.add(btnNewButton);
+		}
+		{
+			JButton btnNewButton_1 = new JButton("Editar");
+			btnNewButton_1.setBounds(182, 202, 84, 20);
+			contentPanel.add(btnNewButton_1);
+		}
+		{
+			JButton btnNewButton_2 = new JButton("Borrar");
+			btnNewButton_2.setBounds(342, 202, 84, 20);
+			contentPanel.add(btnNewButton_2);
+		}
 		{
 			JPanel buttonPane = new JPanel();
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
